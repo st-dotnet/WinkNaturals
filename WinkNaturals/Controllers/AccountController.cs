83983d543e849181d3d0ea93439ca7e7d900a4a3
st-dotnet <stdev.net@gmail.com>
2021-12-29 23:51:37 +0530

    Save

--- WinkNaturals/Controllers/AccountController.cs
+++ WinkNaturals/Controllers/AccountController.cs
@@ -158,9 +158,10 @@ namespace WinkNaturals.Controllers
         }
 
         [HttpPost("SetPrimaryAddress")]
-        public async Task<IActionResult> SetPrimaryAddress(AddressType type)
-        { 
-            return Ok(await _shoppingService.SetCustomerPrimaryAddress(Identity.CustomerID, type));
+        public async Task<IActionResult> SetPrimaryAddress(int type)
+        {
+            var addressType = (AddressType)type;
+            return Ok(await _shoppingService.SetCustomerPrimaryAddress(Identity.CustomerID, addressType));
         }
 
         [HttpPost("MakeCreditCArdPrimary")]
